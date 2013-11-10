9647bdbc0ffd34d1d49de8af89d3931ed953ac39
Simon Last <stlast1@gmail.com>
2013-11-10 12:49:52 -0800

    better nav.

--- search.js
+++ search.js
@@ -6,11 +6,11 @@ $(function(){
 			for (var i = data.sets.length - 1; i >= 0; i--) {
 			var set = data.sets[i];
 			var newThing = $('<div class = "set btn btn-default"></div>');
-			newThing.html('<a href = /index.html?' + set.id + '><div class = "setTitle">'+set.title+'</div></a>');
+			newThing.html('<a href = /set/?' + set.id + '><div class = "setTitle">'+set.title+'</div></a>');
 			$("#searchResults").append(newThing);
 		};
 		}
-		
+
 	}
 	var input = $("#box");
 	input.on("input", function(){
@@ -22,7 +22,7 @@ $(function(){
 		else{
 			var setSource = "https://api.quizlet.com/2.0/search/sets?client_id=9FgEJUznNN&whitespace=1&q="+value
 			$.ajax({
-			url: setSource, 
+			url: setSource,
 			success: render,
 			dataType: "jsonp"
 			});
