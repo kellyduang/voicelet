ecdf26290194b2a0f32f9becb053119fa1099800
elshen <flyingcitrusfruits19@gmail.com>
2013-11-09 16:47:59 -0800

    new thing

--- voice.js
+++ voice.js
@@ -1,5 +1,6 @@
 var recognition = new webkitSpeechRecognition();
   recognition.continuous = true;
+  recognition.interimResults = true;
    
   recognition.onresult = function(event) {
     if (typeof(event.results) == 'undefined') {
@@ -7,16 +8,32 @@ var recognition = new webkitSpeechRecognition();
       return;
     }
     
-    var complete = ""
+    var feedbackContainer = $("#feedback");
+    feedbackContainer.empty();
+    var feedbackthing = $("<div></div>");
+    feedbackContainer.append(feedbackthing);
+
     
     for (var i = event.resultIndex; i < event.results.length; ++i) {
       var tr = event.results[i][0].transcript;
-      complete += tr;
+      console.log(tr);
+      var card = $("#currentCard");
+      var value = card.children(".back").html();
+      if(stringDistance(tr, value) < 3){
+        console.log("yes");
+        var forwardButton = $("#goNext");
+        forwardButton.click();
+        feedbackthing.html("yay");
+      }
+      else{
+        feedbackthing.html("Try again.");
+      }
+      
     }
+    setTimeout(function(){
+      feedbackthing.remove()
+    }, 2300);
     
-    console.log(complete);
-    
-
   };
   
   recognition.onend = function(){
@@ -24,6 +41,6 @@ var recognition = new webkitSpeechRecognition();
 
   };
   
-  $("#start").on("click", function(){
-    recognition.start();
-  });
+  
+recognition.start();
+  
