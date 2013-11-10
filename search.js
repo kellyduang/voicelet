236cecb80e59d9b860866534739bc217d41d5cde
angela278 <angela_zhang278@hotmail.com>
2013-11-10 12:31:23 -0800

    merp

--- search.js
+++ search.js
@@ -2,22 +2,30 @@ $(function(){
 	var render = function(data){
 		console.log(data);
 		$("#searchResults").empty();
-		for (var i = data.sets.length - 1; i >= 0; i--) {
+		if (input.val() !== ""){
+			for (var i = data.sets.length - 1; i >= 0; i--) {
 			var set = data.sets[i];
-			var newThing = $('<div class = "set"></div>');
+			var newThing = $('<div class = "set btn btn-default"></div>');
 			newThing.html('<a href = /index.html?' + set.id + '><div class = "setTitle">'+set.title+'</div></a>');
 			$("#searchResults").append(newThing);
 		};
+		}
+		
 	}
 	var input = $("#box");
 	input.on("input", function(){
 		console.log(input.val())
 		var value = input.val();
-		var setSource = "https://api.quizlet.com/2.0/search/sets?client_id=9FgEJUznNN&whitespace=1&q="+value
-		$.ajax({
-		url: setSource, 
-		success: render,
-		dataType: "jsonp"
-		});
+		if (value === ""){
+			$("#searchResults").empty();
+		}
+		else{
+			var setSource = "https://api.quizlet.com/2.0/search/sets?client_id=9FgEJUznNN&whitespace=1&q="+value
+			$.ajax({
+			url: setSource, 
+			success: render,
+			dataType: "jsonp"
+			});
+		}
 	});
 })
